fef19b449d4639133dbe706b9da7b122d4b3601f
Kris Thornton <Kristhornton@ymail.com>
2024-01-01 09:56:15 -0800

    adding to the login test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -10,11 +10,11 @@ const { test, expect } = require('@playwright/test');
  * 2. Assert that the logo is visible on the page
  */
 
-// test('header has logo visible', async ({ page }) => {
-//   await page.goto('https://Bloomtech.com/');
-//   await expect(page.locator('[alt="Logo Bloom Institute of Technology"]')).toBeVisible();
-//   await expect(page.locator('[class*="landing-page-logo"]')).toBeVisible();
-// })
+test('header has logo visible', async ({ page }) => {
+  await page.goto('https://Bloomtech.com/');
+  await expect(page.locator('[alt="Logo Bloom Institute of Technology"]')).toBeVisible();
+  await expect(page.locator('[class*="landing-page-logo"]')).toBeVisible();
+})
 
 
 /**
@@ -32,6 +32,8 @@ test('unable to log in with an invalid email', async ({ page }) => {
   await page.getByText('Login');
   await page.goto('https://login.bloomtech.com/oauth2/default/v1/authorize?scope=openid+profile+email&sessionToken=&response_type=code&client_id=0oaedywvl9Isex5Vd357&redirect_uri=https%3A%2F%2Fapp.bloomtech.com%2Fapi%2Fauth%2Fcallback%2Fokta&state=883c4d11-53ca-4d26-8fe1-b931dfc188cb');
   await page.getByLabel('email').fill('max@test.com');
+  const locator = page.locator('input[type=text]');
+  await expect(locator).toHaveValue('max@test.com');
 })
 
 
